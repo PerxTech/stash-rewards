592dbf1a760f7605471321e8f1b1ea1b51e235a0
Rui Baltazar <rui.p.baltazar@gmail.com>
2022-06-22 16:31:22 +0200

    wrote simple spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'bundler/setup'
+Bundler.setup
+
+require 'stash/rewards' # and any other gems you need
+
+RSpec.configure do |config|
+  # some (optional) config here
+end
