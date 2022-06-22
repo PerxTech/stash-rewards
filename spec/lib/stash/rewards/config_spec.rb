8b3a83a8a94b87e8153ca0cc7c040b80fd6be61c
Rui Baltazar <rui.p.baltazar@gmail.com>
2022-06-22 16:31:37 +0200

    wrote basic test for config

---
+++ spec/lib/stash/rewards/config_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Stash::Rewards::Config do
+  let(:config) { described_class.new({ api_key: '123', authorization: '456', api_domain: 'https://localhost' }) }
+
+  it 'builds the configuration object' do
+    expect(config.api_key).to eq '123'
+    expect(config.authorization).to eq '456'
+    expect(config.api_domain).to eq 'https://localhost'
+  end
+end
