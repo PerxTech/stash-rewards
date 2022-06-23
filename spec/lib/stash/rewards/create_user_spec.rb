390fe9462c07a7114ceffd7fd00b2a507e01e15f
Rui Baltazar <rui.p.baltazar@gmail.com>
2022-06-23 09:27:10 +0200

    fix broken spec

--- spec/lib/stash/rewards/create_user_spec.rb
+++ spec/lib/stash/rewards/create_user_spec.rb
@@ -16,7 +16,14 @@ RSpec.describe Stash::Rewards::CreateUser do
         "lastName": user_identifier,
         "status": 'ACTIVE',
         "email": "#{user_identifier}@mailinator.com",
-        "phoneNumber": '+6511112223'
+        "phoneNumber": '+6511112223',
+        "address": {
+          "countryCode": 'US',
+          "street": 'string',
+          "suburb": 'string',
+          "state": 'string',
+          "postCode": 'string'
+        }
       }
     ]
   end
