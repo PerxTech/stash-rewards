b647449ed597bb7994b724a7ca34e2e6452d0427
Rui Baltazar <rui.p.baltazar@gmail.com>
2022-06-28 10:28:16 +0200

    added getting reward details and using it to get the reward proce

---
+++ lib/stash/rewards/get_reward.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require_relative 'api_wrapper'
+
+module Stash
+  module Rewards
+    class GetReward < ApiWrapper
+      def call(campaign_id:, reward_id:)
+        api_response = api_wrapper.post("campaigns/#{campaign_id}/rewards/#{reward_id}")
+        response = Stash::Rewards::Response.new(api_response)
+        raise Stash::Rewards::Error, response.error_message if response.error?
+
+        response
+      rescue Faraday::Error => e
+        raise Stash::Rewards::Error, e.message
+      end
+    end
+  end
+end
