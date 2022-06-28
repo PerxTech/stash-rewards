0e2aceb2724b2d6c295ef2a645ae11658d7ed9ab
Rui Baltazar <rui.p.baltazar@gmail.com>
2022-06-28 10:28:16 +0200

    added getting reward details and using it to get the reward proce

--- lib/stash/rewards/issue_voucher.rb
+++ lib/stash/rewards/issue_voucher.rb
@@ -5,9 +5,11 @@ require_relative 'api_wrapper'
 module Stash
   module Rewards
     class IssueVoucher < ApiWrapper
-      def call(campaign_id:, user_identifier:, reward_id:)
+      def call(campaign_id:, user_identifier:, reward_id:, quantity: 1)
+        price = reward_price(reward_id)
+
         api_response = api_wrapper.post("campaigns/#{campaign_id}/users/refId_#{user_identifier}/rewards/order") do |req|
-          req.body = order_payload(reward_id)
+          req.body = order_payload(reward_id, quantity, price)
         end
         response = Stash::Rewards::Response.new(api_response)
         raise Stash::Rewards::Error, response.error_message if response.error?
@@ -19,14 +21,22 @@ module Stash
 
       private
 
-      def order_payload(reward_id)
+      def reward_price
+        reward_response = ::Stash::Rewards::GetReward.call(reward_id)
+        response = Stash::Rewards::Response.new(reward_response)
+        raise Stash::Rewards::Error, response.error_message if response.error?
+
+        response.payload.dig('denonminations')[0].dig('price')
+      end
+
+      def order_payload(reward_id, quantity, price)
         {
           "userInformation": {},
           "products": [
             {
               "rewardId": reward_id,
-              "price": 0,
-              "quantity": 0
+              "price": price,
+              "quantity": quantity
             }
           ]
         }.to_json
